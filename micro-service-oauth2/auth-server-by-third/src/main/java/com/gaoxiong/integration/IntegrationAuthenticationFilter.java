22a31309c5381b6966d10d32a9254922d32dd1ab
200506725@qq.com <gaojunchen520>
2019-08-02 13:46:08 +0800

    继承第三方登陆2

--- micro-service-oauth2/auth-server-by-third/src/main/java/com/gaoxiong/integration/IntegrationAuthenticationFilter.java
+++ micro-service-oauth2/auth-server-by-third/src/main/java/com/gaoxiong/integration/IntegrationAuthenticationFilter.java
@@ -4,12 +4,20 @@ import com.gaoxiong.integration.authenticator.IntegrationAuthenticator;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 /**
  * @author gaoxiong
@@ -27,8 +35,42 @@ public class IntegrationAuthenticationFilter extends GenericFilterBean implement
 
     private ApplicationContext applicationContext;
 
+    private RequestMatcher requestMatcher;
+
+    public IntegrationAuthenticationFilter () {
+        this.requestMatcher = new OrRequestMatcher(
+                new AntPathRequestMatcher(OAUTH_TOKEN_URL, HttpMethod.GET.toString()),
+                new AntPathRequestMatcher(OAUTH_TOKEN_URL, HttpMethod.POST.name())
+        );
+    }
+
     @Override
-    public void doFilter ( ServletRequest request, ServletResponse response, FilterChain chain ) throws IOException, ServletException {
+    public void doFilter ( ServletRequest servletRequest,
+                           ServletResponse servletResponse,
+                           FilterChain chain ) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        if (requestMatcher.matches(request)) {
+            //设置集成登录信息
+            IntegrationAuthentication integrationAuthentication = new IntegrationAuthentication();
+            integrationAuthentication.setAuthType(request.getAuthType());
+            integrationAuthentication.setAuthParameters(request.getParameterMap());
+            IntegrationAuthenticationContext.set(integrationAuthentication);
+
+            try {
+                //预处理
+                this.prepare(integrationAuthentication);
+
+                chain.doFilter(request, response);
+
+                //后置处理
+                this.complete(integrationAuthentication);
+            } finally {
+                IntegrationAuthenticationContext.clear();
+            }
+        } else {
+            chain.doFilter(request, response);
+        }
 
     }
 
@@ -36,4 +78,38 @@ public class IntegrationAuthenticationFilter extends GenericFilterBean implement
     public void setApplicationContext ( ApplicationContext applicationContext ) throws BeansException {
 
     }
+
+    private void prepare ( IntegrationAuthentication integrationAuthentication ) {
+        //延迟加载认证器
+        if (this.authenticators == null) {
+            synchronized (this) {
+                Map<String, IntegrationAuthenticator> map = applicationContext.getBeansOfType(IntegrationAuthenticator.class);
+                if (map != null) {
+                    this.authenticators =  map.values();
+                }
+            }
+        }
+
+        if (this.authenticators == null) {
+            authenticators = new ArrayList<>();
+        }
+        for (IntegrationAuthenticator authenticator : authenticators) {
+            if (authenticator.support(integrationAuthentication)) {
+                authenticator.prepare(integrationAuthentication);
+            }
+        }
+    }
+
+    /**
+     * 后置处理
+     * @param integrationAuthentication
+     */
+    private void complete ( IntegrationAuthentication integrationAuthentication ) {
+        for (IntegrationAuthenticator authenticator : authenticators) {
+            if (authenticator.support(integrationAuthentication)) {
+                authenticator.complete(integrationAuthentication);
+            }
+        }
+    }
+
 }
